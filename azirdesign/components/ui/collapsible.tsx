b8b3ed8667737adb5137ec9fb0cd04e506d833c4
NixonMarrasquin <nixonmarras@gmail.com>
2025-09-03 09:01:55 -0500

    feat: Implement Avatar, Button, Card, and Collapsible components with Radix UI integration

--- azirdesign/components/ui/collapsible.tsx
+++ azirdesign/components/ui/collapsible.tsx
@@ -1,3 +1,33 @@
-// Placeholder for Collapsible component
-export const Collapsible = (props: any) => <div {...props} />
-export const CollapsibleTrigger = (props: any) => <button {...props} />
+"use client"
+
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+function Collapsible({
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
+  return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
+}
+
+function CollapsibleTrigger({
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>) {
+  return (
+    <CollapsiblePrimitive.CollapsibleTrigger
+      data-slot="collapsible-trigger"
+      {...props}
+    />
+  )
+}
+
+function CollapsibleContent({
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+  return (
+    <CollapsiblePrimitive.CollapsibleContent
+      data-slot="collapsible-content"
+      {...props}
+    />
+  )
+}
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
