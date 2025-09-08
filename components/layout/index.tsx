8b3dc4ecdf0bb013e5b4774204f107effe06337e
NixonMarrasquin <nixonmarras@gmail.com>
2025-09-08 13:50:56 -0500

    feat: add sidebar properties to AzirLayout component

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -5,10 +5,20 @@ import { ThemeToggle } from "../theme-toggle"
 import { NavUser } from "../nav-user"
 import { AppSidebar } from "../app-sidebar"
 
-export const AzirLayout = ({ children }: { children: React.ReactNode }) => {
+export const AzirLayout = ({
+  children,
+  sidebarSrc = "/semillapp.png",
+  sidebarAlt = "Semillapp Logo",
+  sidebarSpan = "Semillapp",
+}: {
+  children: React.ReactNode
+  sidebarSrc?: string
+  sidebarAlt?: string
+  sidebarSpan?: string
+}) => {
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar src={sidebarSrc} alt={sidebarAlt} span={sidebarSpan} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex w-full items-center justify-between gap-2 px-4">
